beeb9843d8bdc3301c19d1927dfd495bb28c8560
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-05-02 12:28:11 +0300

    Refactor imports (to exec)

--- Worker.py
+++ Worker.py
@@ -1,57 +1,64 @@
+import sys
 from PyQt5.QtCore import pyqtSlot, QObject
 import time
 
 
 class Worker(QObject):
-    def __init__(self, url, timeout, redownload_numbers, do_archive, do_merge, chapter_count, web_parser):
+    def __init__(self, attrs, web_parser):
         super(Worker, self).__init__()
-        self.url = url
-        self.timeout = timeout
-        self.redownload_numbers = redownload_numbers
-        self.do_archive = do_archive
-        self.do_merge = do_merge
-        self.chapter_count = chapter_count
-        self.countOfDeletedSymbols = web_parser.countOfDeletedSymbols
+        self.attrs = {}
+        self.attrs.update(attrs)
+        self.SYMBOLS_FOR_DELETE = web_parser.SYMBOLS_FOR_DELETE
         self.web_parser = web_parser
         self.parser = None
 
         self.sites = [
-            'ac.qq.com', 'bomtoon.com', 'comic.naver.com', 'comico.kr', 'fanfox.net', 'kuaikanmanhua.com',
-            'manga.bilibili.com', 'mangakakalots.com', 'mangareader.to', 'manhuadb.com', 'scansnelo.com',
-            'page.kakao.com', 'rawdevart.com', 'ridibooks.com', 'webmota.com (baozihm.com)', 'webtoons.com'
+            'ac.qq.com',
+            'bomtoon.com',
+            'comic.naver.com',
+            'comico.kr',
+            'fanfox.net',
+            'kuaikanmanhua.com',
+            'manga.bilibili.com',
+            'mangakakalots.com',
+            'mangareader.to',
+            'manhuadb.com',
+            'scansnelo.com',
+            'page.kakao.com',
+            'rawdevart.com',
+            'ridibooks.com',
+            'webmota.com',
+            'webtoons.com'
         ]
-
 
     @pyqtSlot()
     def run(self):
         self.running = True
+        if self.attrs['url'].startswith('file://'):
+            self.attrs['url'] = self.attrs['url'][self.SYMBOLS_FOR_DELETE:]
 
-        if self.url.startswith('file://'):
-            self.url = self.url[self.countOfDeletedSymbols:]
-
-            with open(self.url, "r") as f:
+            with open(self.attrs['url'], "r") as f:
                 urls = f.read().split('\n')
                 if urls[-1] == '\n' or urls[-1] == '': urls.pop()
-        else: urls = [self.url]
+        else:
+            urls = [self.attrs['url']]
 
-        if self.chapter_count == '':
-            self.chapter_count = 1
+        if self.attrs['chapter_count'] == '': self.attrs['chapter_count'] = 1
 
         for url in urls:
-            attrs = {'url': url, 'timeout': self.timeout, 'redownload_numbers': self.redownload_numbers,
-            'do_archive': self.do_archive, 'do_merge': self.do_merge, 'chapter_count': self.chapter_count}
+            attrs = {}
+            attrs.update(self.attrs)
+            attrs['url'] = url
 
             for site in self.sites:
                 _site = site.replace('.', '_')
                 if site in url:
                     exec(f'from parsers.{_site} import {_site}')
-                    exec(f'self.parser = {_site}(self.web_parser.config)')
+                    exec(f'self.parser = {_site}(self.web_parser.config, self.web_parser.log_file)')
                     break
 
-            if not self.parser is None:
-                try:
-                    self.parser.parse(attrs)
-                except Exception as e:
-                    self.web_parser.save_to_log(e)
+            if self.parser:
+                self.parser.parse(attrs)
+
             time.sleep(1)
         self.running = False
