68998f7ca25e9783d1c57685eff6acbdb6dd5c5e
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-05-02 16:10:41 +0300

    Fixed, refactor

--- parsers/kuaikanmanhua_com.py
+++ parsers/kuaikanmanhua_com.py
@@ -1,32 +1,30 @@
 from parsers.basic_parser import basic_parser
+import time
 
 
 class kuaikanmanhua_com(basic_parser):
+    @basic_parser.logging
     def parse(self, attrs):
         self.update_vars(attrs)
-        browser = self.init_browser(headless=True)
+        self.browser = self.init_browser(headless=True)
 
-        try:
-            while True:
-                self.chapter_count -= self.step
-                browser.get(self.url)
-                src = browser.page_source
+        while True:
+            self.attrs['chapter_count'] -= self.attrs['step']
+            self.browser.get(self.attrs['url'])
+            src = self.browser.page_source
+            title, images = self.find_images(src, 'div', 'class', 'imgList')
+            images = [img.get('data-src') for img in images if not img.get('data-src') is None]
 
-                title, images = self.find_images(src, 'div', 'class', 'imgList')
+            self.full_download(images, title)
 
-                images = [img.get('data-src') for img in images if not img.get('data-src') is None]
+            if self.attrs['chapter_count'] > 0:
+                res = self.find_element(src, 'div', 'class', 'AdjacentChapters').find_all('a')[-1]
+                if 'javascript' not in res:
+                    self.attrs['url'] = 'https://www.kuaikanmanhua.com' + res.get('href')
+                    self.save_folder = self.true_save_folder
+                else: break
+            else: break
 
-                self.full_download(images, title)
-
-                if self.chapter_count > 0:
-                    res = self.find_element(src, 'div', 'class', 'AdjacentChapters').find_all('a')[-1]
-                    if 'javascript' not in res:
-                        self.url = 'https://www.kuaikanmanhua.com' + res.get('href')
-                        self.save_folder = self.true_save_folder
-                    else:
-                        break
-                else:
-                    break
-        finally:
-            browser.close()
-            browser.quit()
+        self.browser.close()
+        self.browser.quit()
+        return True
