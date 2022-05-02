9e41623e5c7dbe8205dddc02cbc261db06c027c8
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-05-02 18:06:28 +0300

    Fixed

--- parsers/page_kakao_com.py
+++ parsers/page_kakao_com.py
@@ -1,45 +1,44 @@
 from parsers.basic_parser import basic_parser
+import time
 
 
 class page_kakao_com(basic_parser):
+    @basic_parser.logging
     def parse(self, attrs):
         self.update_vars(attrs)
-        browser = self.init_browser(user=True)
-
-        browser.minimize_window()
-
+        self.browser = self.init_browser(user=True, full_load=False)
         old_title = ''
+        self.browser.switch_to.new_window('tab')
+        time.sleep(1)
+        self.browser.get(self.attrs['url'])
+        self.browser.minimize_window()
 
-        try:
-            browser.execute_script('window.stop();')
-            browser.switch_to.new_window('tab')
-            time.sleep(1)
-            browser.get(self.url)
+        while True:
+            check = self.browser.execute_script("return document.getElementsByClassName('css-88gyaf')[0];")
 
-            while True:
-                check = browser.execute_script("return document.getElementsByClassName('css-88gyaf')[0];")
+            if check is None:
+                time.sleep(1)
+                continue
+            else:
+                self.browser.execute_script('window.stop();')
 
-                if check is None:
-                    time.sleep(1)
-                    continue
+            self.attrs['chapter_count'] -= self.attrs['step']
 
-                self.chapter_count -= self.step
+            title = self.browser.execute_script("return document.getElementsByClassName('titleWrap')[0].children[0].textContent;")
+            if title == old_title: break
 
-                title = browser.execute_script('return document.getElementsByClassName(\'titleWrap\')[0].children[0].textContent;')
-                if title == old_title: break
+            images = self.browser.execute_script(
+                "return document.getElementsByClassName('css-88gyaf')[0].getElementsByTagName('img');")
 
-                images = browser.execute_script(
-                    'return document.getElementsByClassName(\'css-88gyaf\')[0].getElementsByTagName(\'img\');')
+            images = [el.get_attribute('src') for el in images]
+            self.full_download(images, title)
 
-                images = [el.get_attribute('src') for el in images]
+            if self.attrs['chapter_count'] > 0:
+                s_next = "document.getElementsByClassName('linkItem')[3].click();"
+                s_title = "return document.getElementsByClassName('titleWrap')[0].children[0].textContent;"
+                self.try_next_chapter(self.browser, s_next, title, s_title)
+            else: break
 
-                self.full_download(images, title)
-
-                if self.chapter_count > 0:
-                    s_next = "document.getElementsByClassName('linkItem')[3].click();"
-                    s_title = "return document.getElementsByClassName('titleWrap')[0].children[0].textContent;"
-                    self.try_next_chapter(browser, s_next, title, s_title)
-                else: break
-        finally:
-            browser.close()
-            browser.quit()
+        self.browser.close()
+        self.browser.quit()
+        return True
