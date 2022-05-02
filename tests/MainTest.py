a055b76d884ec4223beafc66da36d385426111b3
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-05-02 18:39:27 +0300

    First commit, create tests

--- tests/MainTest.py
+++ tests/MainTest.py
@@ -5,17 +5,28 @@ from tests.test_ac_qq_com import AcqqcomTest
 from tests.test_bomtoon import BomtoonTest
 from tests.test_comic_naver import NaverTest
 from tests.test_comico import ComicoTest
+from tests.test_fanfox import FanfoxTest
+from tests.test_mangakakalot import MangakakalotTest
+from tests.test_kuaikanmanhua import KuaikanmanhuaTest
+from tests.test_mangareader import MangareaderTest
+from tests.test_ridibooks import RidibooksTest
+from tests.test_kakao import KakaoTest
 
 
 if __name__ == '__main__':
     tester = TestSuite()
 
-    # tester.addTest(makeSuite(WebtoonsTest))
-    # tester.addTest(makeSuite(AcqqcomTest))
-    # tester.addTest(makeSuite(BomtoonTest))
-    # tester.addTest(makeSuite(NaverTest))
-    tester.addTest(makeSuite(ComicoTest))
-    # tester.addTest(makeSuite(FanfoxTest))
+    # tester.addTest(makeSuite(WebtoonsTest))           # Completed
+    # tester.addTest(makeSuite(AcqqcomTest))            # Completed
+    tester.addTest(makeSuite(BomtoonTest))
+    # tester.addTest(makeSuite(NaverTest))              # Completed
+    # tester.addTest(makeSuite(ComicoTest))             # Completed
+    # tester.addTest(makeSuite(FanfoxTest))             # Completed
+    # tester.addTest(makeSuite(MangakakalotTest))       # Completed
+    # tester.addTest(makeSuite(KuaikanmanhuaTest))      # Completed
+    # tester.addTest(makeSuite(MangareaderTest))        # Completed
+    # tester.addTest(makeSuite(RidibooksTest))          # Completed
+    # tester.addTest(makeSuite(KakaoTest))              # Completed
 
     runner = TextTestRunner(verbosity=2)
     runner.run(tester)
--- tests/test_bomtoon.py
+++ tests/test_bomtoon.py
@@ -6,16 +6,16 @@ from tests.config import config, attrs
 
 class BomtoonTest(TestCase):
     def setUp(self):
-        self.site = bomtoon_com(config)
+        self.site = bomtoon_com(config, 'trash/log.txt')
         self.site.set_chromedriver_path('../chromedriver')
         self.attrs = attrs
         self.attrs['url'] = 'https://www.bomtoon.com/comic/ep_view/tlawkddprp/01'
 
-    def tearDown(self):
-        for file in os.listdir('trash'):
-            file = os.path.join('trash', file)
-            if os.path.isfile(file): os.remove(file)
-            else: shutil.rmtree(file)
+    # def tearDown(self):
+    #     for file in os.listdir('trash'):
+    #         file = os.path.join('trash', file)
+    #         if os.path.isfile(file): os.remove(file)
+    #         else: shutil.rmtree(file)
 
     def test_one(self):
         self.assertEqual(self.site.parse(self.attrs), True)
