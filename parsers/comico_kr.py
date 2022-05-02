8f0c31f6dfa1ee36c4281cdd752026c30ea45161
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-05-02 15:51:12 +0300

    Fixed, refactor

--- parsers/comico_kr.py
+++ parsers/comico_kr.py
@@ -1,76 +1,73 @@
 from parsers.basic_parser import basic_parser
+import time
 
 
 class comico_kr(basic_parser):
+    @basic_parser.logging
     def parse(self, attrs):
         self.update_vars(attrs)
-
-        browser = self.init_browser(user=True)
-
+        self.browser = self.init_browser(user=True)
         old_title = ''
 
-        try:
-            browser.execute_script('window.stop();')
-            browser.switch_to.new_window('tab')
-            time.sleep(1)
+        self.browser.switch_to.new_window('tab')
+        time.sleep(1)
+        self.browser.get(self.attrs['url'])
+        self.browser.minimize_window()
 
-            browser.get(self.url)
-            browser.minimize_window()
+        temp_check = -1
 
-            temp_check = -1
+        while True:
+            time.sleep(5)
+            check = self.browser.execute_script('return document.getElementsByClassName(\'viewer_comic\')[0];')
 
-            while True:
-                time.sleep(5)
+            if check is None:
+                continue
+            elif int(check.get_attribute('childElementCount')) < 1 or int(check.get_attribute('childElementCount')) != temp_check:
+                temp_check = int(check.get_attribute('childElementCount'))
+                continue
 
-                check = browser.execute_script('return document.getElementsByClassName(\'viewer_comic\')[0];')
+            self.attrs['chapter_count'] -= self.attrs['step']
 
-                if check is None:
-                    continue
-                elif int(check.get_attribute('childElementCount')) < 1 or int(check.get_attribute('childElementCount')) != temp_check:
-                    temp_check = int(check.get_attribute('childElementCount'))
-                    continue
+            title = self.browser.execute_script("return document.getElementsByClassName('tit_comic')[0].textContent;")
+            if title == old_title: break
 
-                self.chapter_count -= self.step
+            self.current_title = title
+            self.total_images = int(self.browser.execute_script("return document.getElementsByClassName('viewer_comic')[0].children.length;"))
+            self.total_download_images = 0
 
-                title = browser.execute_script('return document.getElementsByClassName(\'tit_comic\')[0].textContent;')
-                if title == old_title: break
+            now_downloaded = len(list(filter(lambda x: '?Policy=' in x.url, self.browser.requests)))
 
-                self.current_title = title
-                self.total_images = int(browser.execute_script("return document.getElementsByClassName('viewer_comic')[0].children.length;"))
-                self.total_download_images = 0
+            while now_downloaded < self.total_images:
+                now_downloaded = len(set(filter(lambda x: '?Policy=' in x.url, self.browser.requests)))
+                self.total_download_images = now_downloaded
+                time.sleep(1)
 
-                now_downloaded = len(list(filter(lambda x: '?Policy=' in x.url, browser.requests)))
+            time.sleep(3)
 
-                while now_downloaded < self.total_images:
-                    now_downloaded = len(list(filter(lambda x: '?Policy=' in x.url, browser.requests)))
-                    self.total_download_images = now_downloaded
-                    time.sleep(1)
+            reqs = set(filter(lambda x: '?Policy=' in x.url, self.browser.requests))
+            images_in_bytes = []
 
-                time.sleep(3)
+            images = self.browser.execute_script("return document.getElementsByClassName('viewer_comic')[0].getElementsByTagName('style');")
+            images = [el.get_attribute('innerHTML') for el in images]
+            images_names = [el[el.index('https://') : el.index('\');')] for el in images if '0px' not in el.split()]
 
-                reqs = list(filter(lambda x: '?Policy=' in x.url, browser.requests))
-                images_in_bytes = []
+            reqs = list(reqs)
+            for i in range(self.total_images):
+                for j in range(len(reqs)):
+                    if images_names[i] in reqs[j].url:
+                        images_in_bytes.append(reqs[j].response.body)
+                        reqs.pop(j)
+                        break
 
-                images = browser.execute_script('return document.getElementsByClassName(\'viewer_comic\')[0].getElementsByTagName(\'style\');')
-                images = [el.get_attribute('innerHTML') for el in images]
-                images_names = [el[el.index('https://') : el.index('\');')] for el in images if '0px' not in el.split()]
+            self.save_images_from_bytes(images_in_bytes, title)
+            del self.browser.requests
 
-                for i in range(self.total_images):
-                    for j in range(len(reqs)):
-                        if images_names[i] in reqs[j].url:
-                            images_in_bytes.append(reqs[j].response.body)
-                            reqs.pop(j)
-                            break
+            if self.attrs['chapter_count'] > 0:
+                s_next = "document.getElementsByClassName('link_next')[0].click();"
+                s_title = "return document.getElementsByClassName('tit_comic')[0].textContent;"
+                self.try_next_chapter(self.browser, s_next, title, s_title)
+            else: break
 
-                self.save_images_from_bytes(images_in_bytes, title)
-
-                if self.chapter_count > 0:
-                    s_next = "document.getElementsByClassName('link_next')[0].click();"
-                    s_title = "return document.getElementsByClassName('tit_comic')[0].textContent;"
-                    self.try_next_chapter(browser, s_next, title, s_title)
-                else: break
-        except Exception as e:
-            print(e)
-        finally:
-            browser.close()
-            browser.quit()
+        self.browser.close()
+        self.browser.quit()
+        return True
