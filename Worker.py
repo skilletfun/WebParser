3613c268953492319dfa4b63ceab71f1f09961ef
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-08-31 14:42:30 +0300

    Refactor

--- Worker.py
+++ Worker.py
@@ -1,13 +1,15 @@
 import sys
-from PyQt5.QtCore import pyqtSlot, QObject
 import time
+import traceback
+
+from PyQt5.QtCore import pyqtSlot, QObject
 
 
 class Worker(QObject):
     def __init__(self, attrs, web_parser):
         super(Worker, self).__init__()
-        self.attrs = {}
-        self.attrs.update(attrs)
+
+        self.attrs = attrs
         self.SYMBOLS_FOR_DELETE = web_parser.SYMBOLS_FOR_DELETE
         self.web_parser = web_parser
         self.parser = None
@@ -23,6 +25,7 @@ class Worker(QObject):
             'mangakakalot.com',
             'mangareader.to',
             'manhuadb.com',
+            'mechacomic.jp',
             'scansnelo.com',
             'page.kakao.com',
             'rawdevart.com',
@@ -35,6 +38,7 @@ class Worker(QObject):
     def run(self):
         try:
             self.running = True
+
             if self.attrs['url'].startswith('file://'):
                 self.attrs['url'] = self.attrs['url'][self.SYMBOLS_FOR_DELETE:]
 
@@ -44,26 +48,27 @@ class Worker(QObject):
             else:
                 urls = [self.attrs['url']]
 
-            if self.attrs['chapter_count'] == '': self.attrs['chapter_count'] = 1
+            if self.attrs['chapter_count'] == '':
+                self.attrs['chapter_count'] = 1
 
             for url in urls:
-                attrs = {}
-                attrs.update(self.attrs)
-                attrs['url'] = url
-
-                for site in self.sites:
-                    _site = site.replace('.', '_')
-                    if site in url:
-                        exec(f'from parsers.{_site} import {_site}')
-                        exec(f'self.parser = {_site}(self.web_parser.config, self.web_parser.log_file)')
-                        break
-
-                if self.parser:
-                    self.parser.parse(attrs)
-
-                time.sleep(1)
+                self.parse(url)
             self.running = False
         except Exception as e:
-            import traceback
-            with open(self.web_parser.log_file, 'a') as file:
-                traceback.print_exc(file=file)
+            raise e
+
+    def parse(self, url):
+        attrs = self.attrs
+        attrs['url'] = url
+
+        for site in self.sites:
+            _site = site.replace('.', '_')
+            if site in url:
+                exec(f'from parsers.{_site} import {_site}')
+                exec(f'self.parser = {_site}(self.web_parser.config, self.web_parser.log_file)')
+                break
+
+        if self.parser:
+            self.parser.parse(attrs)
+
+        time.sleep(1)
