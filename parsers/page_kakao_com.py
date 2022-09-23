1c9e88a39e6b7bd579e8826481961a3ec2f6ffdd
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-09-23 20:11:17 +0300

    Fix with changes of kakao site

--- parsers/page_kakao_com.py
+++ parsers/page_kakao_com.py
@@ -1,12 +1,12 @@
 from parsers.basic_parser import basic_parser
 import time
-
+# https://page.kakao.com/content/59033754/viewer/59046495
 
 class page_kakao_com(basic_parser):
     @basic_parser.logging
     def parse(self, attrs):
         self.update_vars(attrs)
-        self.browser = self.init_browser(user=True, full_load=False)
+        self.browser = self.init_browser(user=True, full_load=True)
         old_title = ''
         self.browser.switch_to.new_window('tab')
         time.sleep(1)
@@ -14,28 +14,27 @@ class page_kakao_com(basic_parser):
         self.browser.minimize_window()
 
         while True:
-            check = self.browser.execute_script("return document.getElementsByClassName('css-88gyaf')[0];")
+            check = self.browser.execute_script("return document.getElementsByClassName('css-3q7n7r-ScrollImageViewerImage')[0];")
 
             if check is None:
                 time.sleep(1)
                 continue
-            else:
-                self.browser.execute_script('window.stop();')
 
             self.attrs['chapter_count'] -= self.attrs['step']
 
-            title = self.browser.execute_script("return document.getElementsByClassName('titleWrap')[0].children[0].textContent;")
+            title = self.browser.execute_script("return document.title;")
             if title == old_title: break
 
             images = self.browser.execute_script(
-                "return document.getElementsByClassName('css-88gyaf')[0].getElementsByTagName('img');")
+                "return document.getElementsByClassName('css-3q7n7r-ScrollImageViewerImage');")
 
             images = [el.get_attribute('src') for el in images]
+            print(images, sep='\n')
             self.full_download(images, title)
 
             if self.attrs['chapter_count'] > 0:
                 s_next = "document.getElementsByClassName('linkItem')[3].click();"
-                s_title = "return document.getElementsByClassName('titleWrap')[0].children[0].textContent;"
+                s_title = "return document.title;"
                 self.try_next_chapter(self.browser, s_next, title, s_title)
             else: break
 
