280e8bdd1b0e8a7fa9d99b262192a12314f076e3
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-10-04 16:53:48 +0300

    Change config format from json to py

---
+++ config.py
@@ -0,0 +1,24 @@
+# Если у вас Windows, установите число 8, если Linux, установите число 7
+SYMBOLS_FOR_DELETE = 8
+
+# Папка для сохранения сканов
+# Внутри указанной папки будет создана еще папка с названием скачиваемой главы
+SAVE_FOLDER = "C:\\Users\\user\\Downloads"
+
+# Путь до браузера на движке Chromium (Chrome, Chromium, Canary, Vivaldi...)
+PATH_TO_BROWSER = "C:\\Users\\user\\AppData\\Local\\Chromium\\User Data\\Default"
+
+# Посылать ли уведомленя о завершении работы (True / False)
+NOTIFY = True
+
+# Сколько за раз запрашивается сканов
+REQUESTS_LIMIT = 10
+
+# Сколько раз программе пытаться скачать один и тот же скан при ошибке 
+DOWNLOAD_TRIES = 10
+
+# Задержка в секундах для скролла страницы
+SCROOL_DELAY = 0.2
+
+# Версия программы
+VERSION = "v1.8"
